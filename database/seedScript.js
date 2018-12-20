e16407287dca797f9b483b2d199513ed3dc32b02
Charlie <cbenz88@gmail.com>
2018-12-19 19:53:40 -0500

    corrected mock data

--- database/seedScript.js
+++ database/seedScript.js
@@ -3,21 +3,42 @@ var mysql = require('mysql');
 var connection = require('./index.js');
 
  var dataInsertCritics = function(){
-    fs.readFile('critics.json', 'utf8', (err, data)=> {
+    fs.readFile(__dirname+'/critics.json','utf-8', (err, data)=> {
         if(err) {
             console.log('Error importing data', err)
         } else {
             var critic = JSON.parse(data);
-            for (var i = 0; i < critic[i].lenght; i++) {
-                connection.query(`INSERT INTO \`critics\` (name, topCritic, publisher, picture) VALUES (?,?,?,?);`, [critic[i].firstname, critic[i].topCritic, critic[i].publisher, critic[i].picture], function(err) {
-        if(err) {
-            console.log(err)
-        } else {
-            console.log('Saved!')
-        }
-    })
+            for (var i = 0; i < critic.length; i++) {
+                connection.query(`INSERT INTO \`critics\` (name, topCritic, publisher, picture) VALUES (?,?,?,?);`, [critic[i].name, Number(critic[i].topCritic), critic[i].publisher, critic[i].picture], function(err) {
+                        if(err) {
+                            console.log('Error saving to database', err)
+                        } else {
+                            console.log('Saved!')
+                        }
+                    })
             }
         }
     });
  }
+
+ var dataInsertReviews = function(){
+    fs.readFile(__dirname+'/reviews.json','utf-8', (err, data)=> {
+        if(err) {
+            console.log('Error importing data', err)
+        } else {
+            var review = JSON.parse(data);
+            for (var i = 0; i < review.length; i++) {
+                connection.query(`INSERT INTO \`reviews\` (criticId, text, rating, movieId, date) VALUES (?,?,?,?,?);`, [review[i].criticId, review[i].text, review[i].rating, review[i].movieId, review[i].date], function(err) {
+                        if(err) {
+                            console.log('Error saving to database', err)
+                        } else {
+                            console.log('Saved!')
+                        }
+                    })
+            }
+        }
+    });
+ }
+
  dataInsertCritics();
+ dataInsertReviews();
