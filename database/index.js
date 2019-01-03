37b00ca82897c64fb917da6fb3e75215b6639305
Charlie <cbenz88@gmail.com>
2019-01-03 17:11:30 -0500

    added front end optimization

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: 'localhost',
   user     : 'root',
-  password : process.env.dbpassword,
+  password : '',
   database : 'FEC'
 
 });
--- server/server.js
+++ server/server.js
@@ -5,9 +5,10 @@ const port = 9003;
 const app = express();
 const router = require("../router/router.js");
 const path = require("path");
+var compression = require('compression');
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, "../client/dist")));
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
