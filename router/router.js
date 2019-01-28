26c136849eac421743aa5ad0084c15f10dafeaa6
Jonathan Raitses <jonathan.raitses@gmail.com>
2019-01-28 14:45:17 -0500

    added redis

--- router/router.js
+++ router/router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const controller = require('../controllers/controllers.js');
+
 router.get('/:number', controller.getReviews);
 
 module.exports = router;
